ded390561fb2705baa051c71c1df28a38b4bbae7
Jean-Yves Tinevez <jean-yves.tinevez@pasteur.fr>
2018-03-23 17:37:33 +0100

    Release v1.0.0.

--- src/main/java/fiji/plugin/trackmate/exporter/csv/TrackMateExporter.java
+++ src/main/java/fiji/plugin/trackmate/exporter/csv/TrackMateExporter.java
@@ -61,7 +61,7 @@ import ome.xml.model.primitives.PositiveInteger;
 
 public class TrackMateExporter
 {
-	public static final String PLUGIN_VERSION = "0.2.0-SNAPSHOT";
+	public static final String PLUGIN_VERSION = "1.0.0";
 
 	private static final DateFormat DATE_FORMAT = new SimpleDateFormat( "yyyy-MM-dd --- HH:mm:ss" );
 
