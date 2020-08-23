be73dc3b669a3d3aa471fd4ec647a45ee4d392a8
Jean-Yves TINEVEZ <jean-yves.tinevez@pasteur.fr>
2020-08-23 10:59:50 +0200

    Catch and print exception when the plugin is launched.

--- src/main/java/fiji/plugin/trackmate/exporter/csv/TrackMateExporterPlugin_.java
+++ src/main/java/fiji/plugin/trackmate/exporter/csv/TrackMateExporterPlugin_.java
@@ -1,14 +1,9 @@
 package fiji.plugin.trackmate.exporter.csv;
 
 import java.io.File;
-import java.util.Locale;
 
 import javax.swing.SwingUtilities;
-import javax.swing.UIManager;
-import javax.swing.UnsupportedLookAndFeelException;
 
-import ij.IJ;
-import ij.ImageJ;
 import ij.plugin.PlugIn;
 
 public class TrackMateExporterPlugin_ implements PlugIn
@@ -22,23 +17,17 @@ public class TrackMateExporterPlugin_ implements PlugIn
 			@Override
 			public void run()
 			{
-				final ExporterController controller = new ExporterController();
-				if ( null != arg && !arg.isEmpty() )
-					controller.setCSVFile( new File( arg ) );
+				try
+				{
+					final ExporterController controller = new ExporterController();
+					if ( null != arg && !arg.isEmpty() )
+						controller.setCSVFile( new File( arg ) );
+				}
+				catch ( final Exception e )
+				{
+					e.printStackTrace();
+				}
 			}
 		} );
-	}
-
-	public static void main( final String[] args ) throws ClassNotFoundException, InstantiationException, IllegalAccessException, UnsupportedLookAndFeelException
-	{
-		Locale.setDefault( Locale.US );
-		UIManager.setLookAndFeel( UIManager.getSystemLookAndFeelClassName() );
-		ImageJ.main( args );
-
-		final File csvFile = new File( "samples/data.csv" );
-		final File impFile = new File( "samples/171004-4mins-tracking.tif" );
-		IJ.openImage( impFile.getAbsolutePath() ).show();
-
-		new TrackMateExporterPlugin_().run( csvFile.getAbsolutePath() );
 	}
 }
